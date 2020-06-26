9d0005b3ec7f79c49c7809fe263c81c32e68ac99
iida l <iidaliina.u@gmail.com>
2020-06-26 17:54:56 +0300

    week 4 first lecturenotes

---
+++ osa4/note-app-backend/build/precache-manifest.a721959f39d352a09d6e4442a2957748.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ddf6b1e172a08d9d2ea1e23bc6916f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62c4c549dc13ece33781",
+    "url": "/static/css/main.db1e9374.chunk.css"
+  },
+  {
+    "revision": "672955b71b49def51d3e",
+    "url": "/static/js/2.255b9624.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.255b9624.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62c4c549dc13ece33781",
+    "url": "/static/js/main.ef49cc6a.chunk.js"
+  },
+  {
+    "revision": "8b4d83dd471ee6ea4d1f",
+    "url": "/static/js/runtime-main.f5e9c1dd.js"
+  }
+]);
